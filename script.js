5cf3c2e1771073de7a4ef53b10971ded718fb7ed
sujanakhanal <sujanakhanal44@gmail.com>
2026-03-13 13:26:22 +0545

    feat: implement add transaction functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const darkToggle = document.querySelector(".navright-part i");
+
 darkToggle.addEventListener("click", (e) => {
   e.preventDefault();
   document.body.classList.toggle("dark-mode");
@@ -25,3 +26,36 @@ if (localStorage.getItem("theme") === "dark") {
   darkToggle.classList.remove("fa-moon");
   darkToggle.classList.add("fa-sun");
 }
+
+const amountInput = document.getElementById("amount-input");
+const categoryInput = document.getElementById("category-input");
+const descriptionInput = document.getElementById("description-input");
+const dateInput = document.getElementById("date-input");
+const addBtn = document.getElementById("add-transaction-btn");
+let transactions = [];
+const savedData = localStorage.getItem("transactions");
+if (savedData) {
+  transactions = JSON.parse(savedData);
+}
+
+addBtn.addEventListener("click", () => {
+  const amount = amountInput.value;
+  const category = categoryInput.value;
+  const description = descriptionInput.value;
+  const date = dateInput.value;
+
+  const transaction = {
+    amount: amount,
+    category: category,
+    description: description,
+    date: date,
+  };
+  transactions.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+  console.log(transactions);
+
+  amountInput.value = "";
+  categoryInput.value = "";
+  descriptionInput.value = "";
+  dateInput.value = "";
+});
